deccba3f650be0b5ece2ba327c3ba11f796b2383
Harry Hodgins <hodginsh@tcd.ie>
2024-08-27 11:55:19 +0100

    Updated run scripts

--- mpk/scaling_tests_pa1v1.sh
+++ mpk/scaling_tests_pa1v1.sh
@@ -12,13 +12,12 @@ for n in "${cores[@]}"; do
 #SBATCH -t 1-03:00:00  # 1 day and 3 hours
 #SBATCH -p compute  # partition name
 #SBATCH -J pa1v1_tests_$n  # job name with core count
-#SBATCH --output=./slurm_files/pa1v1${n}_%j.out  # Output file name
+#SBATCH --output=./slurm_files/pa1v1_${n}_%j.out  # Output file name
 
 # Load necessary modules
 module load openmpi/4.1.6-gcc-8.5.0-fjgly4p tbb compiler-rt intel_ipp_intel64/latest mkl/2024.1
 
 # Change directory
-cd /home/users/mschpc/2023/hodginsh/hpcproject/mpk
 
 EOF
 
