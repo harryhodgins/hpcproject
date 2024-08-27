893d95709e56ac9096d52f2a1d3744fa981e552f
Harry Hodgins <hodginsh@tcd.ie>
2024-08-27 11:16:10 +0100

    Updated TSQR, added run script

---
+++ catsqr/tsqr.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#SBATCH -n 32  # cores
+#SBATCH -t 1-03:00:00   # 1 day and 3 hours
+#SBATCH -p compute      # partition name
+#SBATCH -J tsqr  # sensible name for the job
+#SBATCH --output=./slurm_files/tsqr_32_%j.out  # Output file name, %j will be replaced by the job ID
+
+
+# [load up the correct modules, if required](#load-up-the-correct-modules-if-required)
+module load openmpi/4.1.6-gcc-8.5.0-fjgly4p intel_ipp_intel64/latest mkl/2024.1
+
+cd /home/users/mschpc/2023/hodginsh/hpcproject/catsqr
+
+# [launch the code](#launch-the-code)
+#mpirun -np $SLURM_NTASKS ./tsqr
+mpirun ./tsqr
